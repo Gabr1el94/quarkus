8a1ba20ff89fdb9e3f1c8a23190068d43a80467a
Gabr1el94 <bielsystem64@gmail.com>
2022-11-28 21:35:23 -0300

    config initial helloworld

---
+++ helloworld/src/main/java/acc/br/GreetingResource.java
@@ -0,0 +1,23 @@
+package acc.br;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello RESTEasy";
+    }
+
+    @GET
+    @Path("/eu")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String eu() {
+        return "Olá, eu sou Gabriel!";
+    }
+}
