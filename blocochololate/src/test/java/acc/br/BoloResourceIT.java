0f6bf9eb88a0e390f26a11da6f8d5951eee3e04b
Gabr1el94 <bielsystem64@gmail.com>
2022-12-03 22:22:12 -0300

    delete project

--- blocochololate/src/test/java/acc/br/BoloResourceIT.java
+++
@@ -1,8 +0,0 @@
-package acc.br;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class BoloResourceIT extends BoloResourceTest {
-    // Execute the same tests but in packaged mode.
-}
